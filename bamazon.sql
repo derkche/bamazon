89e5401a76fab498d40b1e33d021fba5b6020d45
derkche <derekchee.40sw@gmail.com>
2018-07-24 14:15:36 -0400

    _-_

--- bamazon.sql
+++ bamazon.sql
@@ -14,3 +14,7 @@ CREATE TABLE products(
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES("Exploding Kittens Card Game", "Toys and Games", 19.99, 37), ("What Do You Meme?", "Toys and Games", 29.99, 54), ("Champion Men's Long Shorts", "Clothing", 12.50, 60), ("Instant Pot Duo Mini Pressure Cooker", "Home and Kitchen", 79.99, 33), ("Nintendo Switch", "Video Games", 299.95, 574), ("Super Smash Bros.", "Video Games", 59.99, 0), ("Pokemon: Lets Go, Pikachu!", "Video Games", 59.99, 304), ("Nike Performance Crew Sock", "Sports and Outdoors", 20.00, 5001), ("RXBAR Whole Food Protein Bar", "Grocery", 20.39, 760), ("Dell Desktop Computer Package with WiFi, Dual Core 2.0GHz, 80GB, 2GB, Windows 10 Professional, 17inch Monitor (Certified Refurbished)", "Electronics", 134.97, 32);
+
+-- use this command to set stock quantities =-=-=-=-=-=-=-=
+UPDATE products Set stock_quantity = ? WHERE item_id = ?
+-- =-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
--- bamazonCustomerIsAlwaysRight.js
+++ bamazonCustomerIsAlwaysRight.js
@@ -13,4 +13,88 @@ connection.connect(function(err){
     if (err) throw err;
     console.log("Connected as ID: " + connection.threadId);
     // run functions here
+    startProgram();
 });
+
+function startProgram(){
+    connection.query("SELECT * FROM products", function(err, res){
+        // console.log(res);
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+        }
+        console.log("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        console.log("What would you like to purchase today?");
+        runDialogue();
+    });
+}
+
+function runDialogue(){
+    inquirer.prompt([
+        {
+            name: "chosenId",
+            type: "rawinput",
+            message: "Please select your product via its ID number!"
+        },
+        {
+            name: "chosenQuantity",
+            type: "rawinput",
+            message: "How many would you like?"
+        }
+    ]).then(function(inqResponse){
+
+        // console.log(inqResponse);
+        // console.log("Chosen Product Id: "+inqResponse.chosenId); 
+        // console.log("Chosen Product Quantity: "+inqResponse.chosenQuantity);
+        var chosenID = inqResponse.chosenId;
+        var chosenQuantity = inqResponse.chosenQuantity;
+        // console.log("var chosenID: "+chosenID); 
+        console.log("var chosenQuantity: "+chosenQuantity);
+
+
+        connection.query("SELECT * FROM products WHERE item_id = "+chosenID+"", function(err, itemCheck){
+            
+            // console.log("In-Stock Quantity: " + itemCheck[0].stock_quantity);
+            var stockQuantity = itemCheck[0].stock_quantity;
+            // console.log("var stockQuantity: "+stockQuantity);
+
+            if (chosenQuantity > stockQuantity){
+
+                console.log("Sorry, we dont have enough in stock to complete your order :(");
+
+            }else if (chosenQuantity <= stockQuantity) {
+
+                console.log("Fetching your order now...");
+
+                var updateQuantity = stockQuantity - chosenQuantity;
+                // console.log("Pending Database Quantity " + updateQuantity);
+
+                connection.query("UPDATE products Set stock_quantity = "+ updateQuantity +" WHERE item_id = "+ chosenID +"", function(err, update){
+                    if (err) throw err;
+                    // console.log(update);
+                    // console.log("Database Message: "+update.message);
+                    // console.log("Database Message: Affected Rows:"+update.affectedRows);
+                    if (update.affectedRows>0){
+
+                        // console.log("Database update was successful");
+                        console.log("Item Price: "+itemCheck[0].price);
+                        console.log("Your total for this transaction is $"+itemCheck[0].price*chosenQuantity);
+
+                        inquirer.prompt([{
+                            name: 'shoporquit',
+                            type: 'list',
+                            message: 'Is that all for today?',
+                            choices: ["Keep shopping", "Sign Off"]
+                        }]).then(function(followupInq){
+                            if(followupInq.shoporquit = "Keep shopping"){
+                                startProgram();
+                            }else if (followupInq.shoporquit = "Sign Off"){
+                                process.exit();
+                            }
+                        });
+
+                    };
+                });
+            };
+        });
+    });
+};
