ece8dcd82aff3b43f1eb37ea8eac4852e0c1727b
derkche <derekchee.40sw@gmail.com>
2018-07-24 14:34:46 -0400

    __----___

--- bamazonPrime.js
+++ bamazonPrime.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -48,7 +49,7 @@ function runDialogue(){
         var chosenID = inqResponse.chosenId;
         var chosenQuantity = inqResponse.chosenQuantity;
         // console.log("var chosenID: "+chosenID); 
-        console.log("var chosenQuantity: "+chosenQuantity);
+        // console.log("var chosenQuantity: "+chosenQuantity);
 
 
         connection.query("SELECT * FROM products WHERE item_id = "+chosenID+"", function(err, itemCheck){
@@ -57,9 +58,15 @@ function runDialogue(){
             var stockQuantity = itemCheck[0].stock_quantity;
             // console.log("var stockQuantity: "+stockQuantity);
 
-            if (chosenQuantity > stockQuantity){
+            if(chosenID === "6"){
+                console.log("Sorry, that item hasn't been released yet...");
+                console.log("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+                startProgram();
+            }else if (chosenQuantity > stockQuantity){
 
                 console.log("Sorry, we dont have enough in stock to complete your order :(");
+                console.log("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+                startProgram();
 
             }else if (chosenQuantity <= stockQuantity) {
 
@@ -76,9 +83,12 @@ function runDialogue(){
                     if (update.affectedRows>0){
 
                         // console.log("Database update was successful");
-                        console.log("Item Price: "+itemCheck[0].price);
+                        // console.log("Item Price: "+itemCheck[0].price);
+                        console.log(itemCheck[0].product_name);
                         console.log("Your total for this transaction is $"+itemCheck[0].price*chosenQuantity);
+                        console.log("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
 
+                        'use strict';
                         inquirer.prompt([{
                             name: 'shoporquit',
                             type: 'list',
